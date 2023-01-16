471434c435f4915b72118485d670b4f8fcba2277
Kevin Machava <kevinmachava20@gmail.com>
2023-01-16 11:40:00 +0200

    Update axios.js

--- quasar-auth-breeze-api/src/boot/axios.js
+++ quasar-auth-breeze-api/src/boot/axios.js
@@ -9,6 +9,8 @@ import axios from 'axios'
 // for each client)
 
 const api = axios.create({ baseURL: 'http://127.0.0.1:8000' })
+//api.defaults.xsrfCookieName = 'csrftoken'
+//api.defaults.xsrfHeaderName = 'X-CSRFToken'
 //api.defaults.headers.common['Authorization'] = 'Bearer '
 axios.defaults.withCredentials=true
 
